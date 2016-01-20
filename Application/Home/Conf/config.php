f5ba6d09be2ef5c9933d62bd5684f9f74596af66
Chien <467360687@qq.com>
2016-01-21 01:17:19 +0800

    init frame

---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'TMPL_PARSE_STRING'      => array('__PUBLIC__'=>'/'),
+    'DB_TYPE'                => 'mysql',
+    'DB_HOST'                => 'localhost',
+    'DB_NAME'                => '',
+    'DB_USER'                => '',
+    'DB_PWD'                 => '',
+);
