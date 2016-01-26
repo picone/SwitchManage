786dc5957f6854dfc4ff675089dfda2bd4104b7f
qqw78901 <qqw78901@126.com>
2016-01-26 20:39:52 +0800

    增加交换机单个管理和批量关引导页

---
+++ Web/js/switchmanage.js
@@ -0,0 +1,25 @@
+/**
+ * Created by King-z on 2016/1/26 0026.
+ */
+$(function () {
+    var chosenData;
+    $("#next").click(function () {
+        var list = [];
+        $("input[name='switch']:checkbox").each(function () {
+            if (this.checked) {
+                list.push(this.value);
+            }
+            chosenData = list.join(",");
+        });
+        alert("选择了" + list.length + "台交换机:" + chosenData);
+
+    });
+
+    /*    jQuery.each(jQuery("input[name='switch']:checkbox"),function (i,item){
+     jQuery(item).change(function(){
+
+     })
+     });*/
+
+
+});
