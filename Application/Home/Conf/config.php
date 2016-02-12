8193bd934d6ad74d6f7be87e8bea2400020d8bf6
Chien <467360687@qq.com>
2016-02-12 23:12:19 +0800

    删掉配置文件=_=

--- Application/Home/Conf/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-    'TMPL_PARSE_STRING'      => array('__PUBLIC__'=>__ROOT__.'/Web'),
-    'DB_TYPE'                => 'mysql',
-    'DB_HOST'                => 'localhost',
-    'DB_NAME'                => '',
-    'DB_USER'                => '',
-    'DB_PWD'                 => '',
-);
